618e8f62c534ba3cc78609d1088551b26b73fb36
allyusd <allyusd@yahoo.com.tw>
2016-09-19 05:25:53 +0800

    Refresh the AppComponent

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,16 +17,17 @@ const HEROES: Hero[] = [
 
 @Component({
   selector: 'my-app',
-  template:`
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles: [`
   .selected {
