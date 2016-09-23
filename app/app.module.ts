d9b4ef3f3b57eb360560c5eb0d85cffb8d72d0eb
allyusd <allyusd@yahoo.com.tw>
2016-09-24 02:37:49 +0800

    Register for HTTP services

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { DashboardComponent }      from './dashboard.component';
 import { HeroService }         from './hero.service';
 import { routing } from './app.routing';
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
